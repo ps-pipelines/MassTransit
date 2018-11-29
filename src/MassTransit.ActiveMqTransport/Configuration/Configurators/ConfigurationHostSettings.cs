e90404b51bd932394382375bb946891c63f7c36f
David Rundle <david.rundle@vwfs.co.uk>
2018-11-29 16:31:25 +0000

    missing using

--- src/MassTransit.ActiveMqTransport/Configuration/Configurators/ConfigurationHostSettings.cs
+++ src/MassTransit.ActiveMqTransport/Configuration/Configurators/ConfigurationHostSettings.cs
@@ -14,6 +14,7 @@ namespace MassTransit.ActiveMqTransport.Configurators
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using Apache.NMS;
     using Apache.NMS.ActiveMQ;
